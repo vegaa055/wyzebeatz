44b55e60796fefa8cd748721f7bd2010228a1f11
vegaa055 <74841685+vegaa055@users.noreply.github.com>
2025-05-16 15:56:06 -0700

    added new songs to lib page

--- scripts/library_tracks.js
+++ scripts/library_tracks.js
@@ -1,7 +1,16 @@
 const audioFiles = [
   { id: "waveform0", src: "audio/never_stop.mp3" },
   { id: "waveform1", src: "audio/the_journey.mp3" },
-  { id: "waveform2", src: "audio/neverending_story_vega_REMIX.mp3" },
+  { id: "waveform2", src: "audio/void_trip.mp3" },
+  { id: "waveform3", src: "audio/built_4_this.mp3" },
+  { id: "waveform4", src: "audio/ride_with_me.mp3" },
+  { id: "waveform5", src: "audio/raw.mp3" },
+  { id: "waveform6", src: "audio/all_the_smoke.mp3" },
+  { id: "waveform7", src: "audio/neverending_story_vega_REMIX.mp3" },
+  { id: "waveform8", src: "audio/singularity.mp3" },
+  { id: "waveform9", src: "audio/dystopian_dreamz.mp3" },
+  { id: "waveform10", src: "audio/summer_nights.mp3" },
+  { id: "waveform11", src: "audio/waves.mp3" },
 ];
 
 const waveforms = [];
